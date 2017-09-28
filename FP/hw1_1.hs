cd6f50dc456a96dd823f4c6b5ed2a4726ae1cf34
DariaMelnikova <melnikova_daria@inbox.ru>
2017-09-28 10:43:14 +0300

    lost function added to hw1_1.hs

--- FP/hw1_1.hs
+++ FP/hw1_1.hs
@@ -17,3 +17,8 @@ infixl 7 <*>
 (IntConstant a) <*> (IntConstant b) = IntConstant (a * b)
 a <*> b = BinaryTerm a b Mult
 
+replaceVar :: Term -> String -> Term -> Term
+replaceVar (IntConstant c) _ _ = IntConstant c
+replaceVar (Variable v) name term = if v == name then term else Variable v
+replaceVar (BinaryTerm l r op) name term = BinaryTerm (replaceVar l name term) (replaceVar r name term) op
+replaceVar (UnaryTerm op v) name term = UnaryTerm op (replaceVar v name term)
